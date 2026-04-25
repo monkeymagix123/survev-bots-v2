5556e137daaaf0c17c1b2fa06538fbcda8c0af71
sus <iamsuchabozo12345@gmail.com>
2026-04-25 11:15:08 -0700

    add waveDefs.ts

---
+++ shared/defs/maps/waveDefs.ts
@@ -0,0 +1,19 @@
+import { util } from "../../utils/util";
+import type { MapDef } from "../mapDefs";
+import { Main, type PartialMapDef } from "./baseDefs";
+
+const mapDef: PartialMapDef = {
+    mapId: 11, // start at 11 for new maps
+    desc: {
+        name: "Wave",
+        icon: "img/gui/star.svg", // SHOULD CHANGE THIS
+        buttonCss: "",
+    },
+    gameMode: {
+        maxPlayers: 100,
+        factionMode: true,
+        factions: 2,
+    },
+};
+
+export const Faction = util.mergeDeep({}, Main, mapDef) as MapDef;
