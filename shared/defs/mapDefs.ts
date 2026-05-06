072b0736cdd3cc5b83ebd62a2b7bfe6327afa511
sus <iamsuchabozo12345@gmail.com>
2026-05-05 18:01:40 -0700

    revert original waveDefs and add 2nd wave defs 2 temporarily

--- shared/defs/mapDefs.ts
+++ shared/defs/mapDefs.ts
@@ -12,6 +12,7 @@ import { Savannah } from "./maps/savannahDefs";
 import { Snow } from "./maps/snowDefs";
 import { Turkey } from "./maps/turkeyDefs";
 import { Wave } from "./maps/waveDefs";
+import { Wave2 } from "./maps/waveDefs2";
 import { Woods } from "./maps/woodsDefs";
 import { WoodsSnow } from "./maps/woodsSnowDefs";
 import { WoodsSpring } from "./maps/woodsSpringDefs";
@@ -35,6 +36,7 @@ export const MapDefs = {
     cobalt: Cobalt,
     turkey: Turkey,
     wave: Wave,
+    wave2: Wave2,
 } satisfies Record<string, MapDef>;
 
 export type Atlas =
